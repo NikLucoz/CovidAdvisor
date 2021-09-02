89978f35d528b4f64c15a871000ed193ed8fcd43
NikLucoz <lucozzi.niccolo@gmail.com>
2021-09-02 21:00:20 +0200

    HomePage and Navigation

--- App.tsx
+++ App.tsx
@@ -1,21 +1,35 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View, Image, ScrollView, } from 'react-native';
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+
+import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+
+
+
+//@ts-ignore
+import Home from './src/pages/Home.tsx';
+//@ts-ignore
+import ListaRegioni from './src/pages/ListaRegioni.tsx'
+
+
+const Tab = createBottomTabNavigator();
+const RootNavigator = () => {
+  return (
+    <Tab.Navigator >
+      <Tab.Screen name="Home" component={Home} options = {{headerShown: false, tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="home" color={color} size={size} /> ) }}/>      
+      <Tab.Screen name="Lista Regioni" component={ListaRegioni} options = {{headerShown: false, tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="map-marker-radius" color={color} size={size} /> ) }} />
+    </Tab.Navigator>
+  );
+}; 
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <RootNavigator />
+    </NavigationContainer>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
---
+++ src/pages/ListaRegioni.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { StyleSheet, Text, View, Image, ScrollView, TouchableOpacity } from 'react-native';
+
+export default class Home extends React.Component {
+    render() {
+      return (
+          <View>
+              
+          </View>
+      );
+    }
+}
