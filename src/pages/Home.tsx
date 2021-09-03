d91858a94a99232e5f0ab1f1a28190b3496808ef
NikLucoz <lucozzi.niccolo@gmail.com>
2021-09-03 18:56:56 +0200

    test

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -20,6 +20,8 @@ function get(url: string) {
   });
 }
 
+
+
 async function GetNationalData(){
   
   let data = await get('https://raw.githubusercontent.com/pcm-dpc/COVID-19/master/dati-andamento-nazionale/dpc-covid19-ita-andamento-nazionale-latest.csv');
