5d835d67e9e2b90a61ba110ba1ca7a150283f84d
NikLucoz <lucozzi.niccolo@gmail.com>
2021-09-04 14:32:17 +0200

    Page Done

--- App.tsx
+++ App.tsx
@@ -1,5 +1,5 @@
 import { StatusBar } from 'expo-status-bar';
-import React from 'react';
+import React, { useState } from 'react';
 import { StyleSheet, Text, View, Image, ScrollView, } from 'react-native';
 
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
@@ -7,8 +7,6 @@ import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
-
-
 
 //@ts-ignore
 import Home from './src/pages/Home.tsx';
@@ -18,13 +16,12 @@ import ListaRegioni from './src/pages/ListaRegioni.tsx'
 import InfoRegione from './src/pages/InfoRegione.tsx'
 
 const Tab = createBottomTabNavigator();
+
 const RootNavigator = () => {
   return (
     <Tab.Navigator >
       <Tab.Screen name="Home" component={Home} options = {{headerShown: false, tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="home" color={color} size={size} /> ) }}/>      
-      <Tab.Screen name="Lista Regioni" component={ListaRegioni} options = {{headerShown: false, tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="map-marker-radius" color={color} size={size} /> ) }} />
       <Tab.Screen name="Info Regione" component={InfoRegione} options = {{headerShown: false, tabBarIcon: ({ color, size }) => (<MaterialCommunityIcons name="map-marker-radius" color={color} size={size} /> ) }} />
-
     </Tab.Navigator>
   );
 }; 
--- src/pages/ListaRegioni.tsx
+++ src/pages/ListaRegioni.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { StyleSheet, Text, View, Image, ScrollView, TouchableHighlight, TouchableOpacity } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 
 //@ts-ignore
 import * as regione from './InfoRegione.tsx'
 
 function showRegionalInfo(ID: number) {
   regione.setID(ID);
-
 }
-
-
 export default class ListaRegioni extends React.Component {
+    
     render() {
+      
       return (
         <View style={styles.background}>
         <ScrollView showsVerticalScrollIndicator={false} style={{ width:'100%', height: '100%', }}>
@@ -30,128 +30,128 @@ export default class ListaRegioni extends React.Component {
             </View>
                
 
-            <TouchableHighlight onPress={showRegionalInfo(1)}>
+            <TouchableHighlight onPress={() => showRegionalInfo(1)}>
               <View style={styles.textContainer}>
                 <Text style={{fontSize: 25, marginTop: 15 , fontWeight: '600'}}>Abruzzo</Text>
               </View>
             </TouchableHighlight>
 
-            <TouchableHighlight >
+            <TouchableHighlight onPress={() => showRegionalInfo(2)}>
               <View style={styles.textContainer}>
                 <Text style={{fontSize: 25, marginTop: 15 , fontWeight: '600'}}>Basilicata</Text>
               </View>
             </TouchableHighlight>
             
-            <TouchableHighlight>
+            <TouchableHighlight onPress={() => showRegionalInfo(3)}>
               <View style={styles.textContainer}>
                 <Text style={{fontSize: 25, marginTop: 15 , fontWeight: '600'}}>Calabria</Text>
               </View>
             </TouchableHighlight>
 
-            <TouchableHighlight>
+            <TouchableHighlight onPress={() => showRegionalInfo(4)}>
               <View style={styles.textContainer}>
                 <Text style={{fontSize: 25, marginTop: 15 , fontWeight: '600'}}>Campania</Text>
               </View>
             </TouchableHighlight>
             
-            <TouchableHighlight>
+            <TouchableHighlight onPress={() => showRegionalInfo(5)}>
               <View style={styles.textContainer}>
                 <Text style={{fontSize: 25, marginTop: 15 , fontWeight: '600'}}>Emilia Romagna</Text>
               </View>
             </TouchableHighlight>
             
-            <TouchableHighlight>
+            <TouchableHighlight onPress={() => showRegionalInfo(6)}>
               <View style={styles.textContainer}>
                 <Text style={{fontSize: 25, marginTop: 15 , fontWeight: '600'}}>Friuli Venezia Giulia</Text>
               </View>
             </TouchableHighlight>
             
-            <TouchableHighlight>
+            <TouchableHighlight onPress={() => showRegionalInfo(7)}>
               <View style={styles.textContainer}>
                 <Text style={{fontSize: 25, marginTop: 15 , fontWeight: '600'}}>Lazio</Text>
               </View>
             </TouchableHighlight>
 
-            <TouchableHighlight>
+            <TouchableHighlight onPress={() => showRegionalInfo(8)}>
               <View style={styles.textContainer}>
                 <Text style={{fontSize: 25, marginTop: 15 , fontWeight: '600'}}>Liguria</Text>
               </View>
             </TouchableHighlight>
 
-            <TouchableHighlight>
+            <TouchableHighlight onPress={() => showRegionalInfo(9)}>
               <View style={styles.textContainer}>
                 <Text style={{fontSize: 25, marginTop: 15 , fontWeight: '600'}}>Lombardia</Text>
               </View>
             </TouchableHighlight>
             
-            <TouchableHighlight>
+            <TouchableHighlight onPress={() => showRegionalInfo(10)}>
               <View style={styles.textContainer}>
                 <Text style={{fontSize: 25, marginTop: 15 , fontWeight: '600'}}>Marche</Text>
               </View>
             </TouchableHighlight>
             
-            <TouchableHighlight>
+            <TouchableHighlight onPress={() => showRegionalInfo(11)}>
               <View style={styles.textContainer}>
                 <Text style={{fontSize: 25, marginTop: 15 , fontWeight: '600'}}>Molise</Text>
               </View>
             </TouchableHighlight>
             
-            <TouchableHighlight>
+            <TouchableHighlight onPress={() => showRegionalInfo(12)}>
               <View style={styles.textContainer}>
                 <Text style={{fontSize: 25, marginTop: 15 , fontWeight: '600'}}>Bolzano</Text>
               </View>
             </TouchableHighlight>
             
-            <TouchableHighlight>
+            <TouchableHighlight onPress={() => showRegionalInfo(13)}>
               <View style={styles.textContainer}>
                 <Text style={{fontSize: 25, marginTop: 15 , fontWeight: '600'}}>Trento</Text>
               </View>
             </TouchableHighlight>
             
-            <TouchableHighlight>
+            <TouchableHighlight onPress={() => showRegionalInfo(14)}>
               <View style={styles.textContainer}>
                 <Text style={{fontSize: 25, marginTop: 15 , fontWeight: '600'}}>Piemonte</Text>
               </View>
             </TouchableHighlight>
             
-            <TouchableHighlight>
+            <TouchableHighlight onPress={() => showRegionalInfo(15)}>
               <View style={styles.textContainer}>
                 <Text style={{fontSize: 25, marginTop: 15 , fontWeight: '600'}}>Puglia</Text>
               </View>
             </TouchableHighlight>
             
-            <TouchableHighlight>
+            <TouchableHighlight onPress={() => showRegionalInfo(16)}>
               <View style={styles.textContainer}>
                 <Text style={{fontSize: 25, marginTop: 15 , fontWeight: '600'}}>Sardegna</Text>
               </View>
             </TouchableHighlight>
 
             
-            <TouchableHighlight>
+            <TouchableHighlight onPress={() => showRegionalInfo(17)}>
               <View style={styles.textContainer}>
                 <Text style={{fontSize: 25, marginTop: 15 , fontWeight: '600'}}>Sicilia</Text>
               </View>
             </TouchableHighlight>
             
-            <TouchableHighlight>
+            <TouchableHighlight onPress={() => showRegionalInfo(18)}>
               <View style={styles.textContainer}>
                 <Text style={{fontSize: 25, marginTop: 15 , fontWeight: '600'}}>Toscana</Text>
               </View>
             </TouchableHighlight>
             
-            <TouchableHighlight>
+            <TouchableHighlight onPress={() => showRegionalInfo(19)}>
               <View style={styles.textContainer}>
                 <Text style={{fontSize: 25, marginTop: 15 , fontWeight: '600'}}>Umbria</Text>
               </View>
             </TouchableHighlight>
             
-            <TouchableHighlight>
+            <TouchableHighlight onPress={() => showRegionalInfo(20)}>
               <View style={styles.textContainer}>
                 <Text style={{fontSize: 25, marginTop: 15 , fontWeight: '600'}}>Valle d'Aosta</Text>
               </View>
             </TouchableHighlight>
             
-            <TouchableHighlight>
+            <TouchableHighlight onPress={() => showRegionalInfo(21)}>
               <View style={styles.textContainer}>
                 <Text style={{fontSize: 25, marginTop: 15 , fontWeight: '600'}}>Veneto</Text>
               </View>
